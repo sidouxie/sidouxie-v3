2fe12fd7ce0191eb5be75e69554f2d614ef07984
sidouxie <sid_ouxiee@hotmail.com>
2024-06-22 11:50:05 +0100

    feat: add mocked work posts data static

--- src/api/GetPostsData.js
+++ src/api/GetPostsData.js
@@ -1,14 +1,27 @@
-import axios from "axios";
+// import axios from "axios";
+
+import { worksPostsData } from "../__mock__/WorkPostsData";
+
+// const getPostsData = async () => {
+//   const result = await axios
+//     .get(`${process.env.REACT_APP_API_URL}/api/post`, {
+//       headers: {
+//         "content-type": "application/json",
+//       },
+//     })
+//     .then((response) => response.data);
+//   // .catch((err) => err.response.data);
+//   return result;
+// };
 
 const getPostsData = async () => {
-  const result = await axios
-    .get(`${process.env.REACT_APP_API_URL}/api/post`, {
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-    .then((response) => response.data);
-  // .catch((err) => err.response.data);
+  const result = await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(worksPostsData);
+      reject(console.error("There is some errors to get Data!"));
+    }, 1000);
+  });
+
   return result;
 };
 
